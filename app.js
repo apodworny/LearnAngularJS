af5a5154d0e61929b6e9ee5527ebb27c61d3bf4f
Alex Podworny <alex.podworny@hotmail.com>
2021-01-15 18:36:17 -0600

    Video 42 - created project, added routing for home and forecast pages

---
+++ app.js
@@ -0,0 +1,20 @@
+// Module
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    }).when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+});
+
+// Controllers
+weatherApp.controller('homeController', [$scope, function($scope) {
+
+}]);
+weatherApp.controller('forecastController', [$scope, function($scope) {
+    
+}]);
